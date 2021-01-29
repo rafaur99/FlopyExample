f7828a433ffe0f673245168ee6f822b0dfd2bf91
Rafael Uribe <rafael2170246@correo.uis.edu.co>
2021-01-29 13:22:51 -0500

    primer ploteada de capa

--- Primera_progra.py
+++ Primera_progra.py
@@ -105,6 +105,18 @@ sim.write_simulation()
 success, buff = sim.run_simulation()
 if not success:
     raise Exception("MODFLOW 6 did not terminate normally.")
+ 
+#primer ploteado de las primeras ispiezas   
+headfile= "workspace"+"/"+headfile 
+hds = flopy.utils.binaryfile.HeadFile(headfile)
+h = hds.get_data(kstpkper=(0, 0))
+x = y = np.linspace(0, L, N)
+y = y[::-1]
+fig = plt.figure(figsize=(6, 6))
+ax = fig.add_subplot(1, 1, 1, aspect="equal")
+c = ax.contour(x, y, h[0], np.arange(90, 100.1, 0.2), colors="black")
+plt.clabel(c, fmt="%2.1f")
+    
     
     
 
